1fd225d45369ae75c4a56ef4dd6ed372ea11c029
mohammadhossein <m.h.dolatabadi.a@gmail.com>
2020-10-26 14:26:32 +0330

    add weblite api

--- Client/index.js
+++ Client/index.js
@@ -1,18 +1,18 @@
 import { render } from "./render.js";
 
-let id = 0;
+// let id = 0;
 
-// const W = window.W
-// W.setHooks({
-//     wappWillStart(start){
-//         start()
-//     }
-// })
+const W = window.W
+W.setHooks({
+    wappWillStart(start){
+        start()
+    }
+})
 
 export const roomId = () => {
-    // return W.wapp.getWisId()
-    id++;
-    return id;
+    return W.wapp.getWisId()
+    // id++;
+    // return id;
 }
 
 render();
