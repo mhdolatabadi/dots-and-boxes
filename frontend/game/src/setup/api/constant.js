af607c30da94185cb2a95d927d55028f50b7389e
MohammadHossein <m.h.dolatabadi.a@gmail.com>
2021-05-31 13:17:18 +0430

    fix: Change constants to master branch

--- frontend/game/src/setup/api/constant.js
+++ frontend/game/src/setup/api/constant.js
@@ -1,5 +1,5 @@
 export const HTTP_BACKEND =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:13797'
-    : // 'https://noghteh-bazi.wapp.weblite.me/'
-      'https://noghte-bazi-2.wapp.weblite.me/'
+    : 'https://noghteh-bazi.wapp.weblite.me/'
+// 'https://noghte-bazi-2.wapp.weblite.me/'
